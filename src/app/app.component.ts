1506234b467d6dd34b053fb21e9eede7074863dc
jason zheng <tso1158687@gmail.com>
2024-07-05 00:53:27 +0800

    adjust ask page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,11 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AskService } from './service/ask.service';
-import { FormsModule } from '@angular/forms';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { ButtonModule } from 'primeng/button';
 import { HeaderComponent } from './component/header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    FormsModule,
-
-    ButtonModule,
-    HeaderComponent,
-    RouterOutlet
-  ],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
-  private askService = inject(AskService);
-  title = 'think-therefore-be';
-  currentQuestion = '';
-  answer = '';
-  value = 'taipei';
-  ngOnInit(): void {}
-
-  askQuestion(): void {
-    this.askService.askGemini(this.currentQuestion).subscribe((response) => {
-      console.log(response);
-      this.answer = response;
-    });
-  }
-}
+export class AppComponent {}
--- src/app/page/ask/ask.component.ts
+++ src/app/page/ask/ask.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 import { InputTextareaModule } from 'primeng/inputtextarea';
+import { AskService } from '../../service/ask.service';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-ask',
   standalone: true,
-  imports: [FormsModule,InputTextareaModule,],
+  imports: [
+    FormsModule,
+    InputTextareaModule,
+    ButtonModule,
+    ProgressSpinnerModule,
+  ],
   templateUrl: './ask.component.html',
-  styleUrl: './ask.component.scss'
+  styleUrl: './ask.component.scss',
 })
 export class AskComponent {
-  value=''
+  private askService = inject(AskService);
+  isLoaded = false;
+  currentQuestion = '';
+  answer = '';
 
+  askQuestion(): void {
+    this.isLoaded = true;
+    this.askService
+      .askGemini(this.currentQuestion)
+      .pipe(finalize(() => (this.isLoaded = false)))
+      .subscribe((response) => {
+        console.log(response);
+        this.answer = response;
+      });
+  }
 }
