214fd7aab4ebe86c845fb13f8b6366437e0f907b
jason zheng <tso1158687@gmail.com>
2024-07-10 18:58:22 +0800

    add various precondition

--- src/app/page/ask/ask.component.ts
+++ src/app/page/ask/ask.component.ts
@@ -1,19 +1,26 @@
 import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { AskService } from '../../service/ask.service';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { finalize } from 'rxjs';
-
+import { SelectButtonModule } from 'primeng/selectbutton';
 @Component({
   selector: 'app-ask',
   standalone: true,
   imports: [
-    FormsModule,
     InputTextareaModule,
     ButtonModule,
     ProgressSpinnerModule,
+    SelectButtonModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './ask.component.html',
   styleUrl: './ask.component.scss',
@@ -21,13 +28,26 @@ import { finalize } from 'rxjs';
 export class AskComponent {
   private askService = inject(AskService);
   isLoaded = false;
-  currentQuestion = '';
   answer = '';
+  askForm = new FormGroup({
+    question: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(1),
+    ]),
+    precondition: new FormControl('a', [Validators.required]),
+  });
+
+  preconditionOptions: any[] = [
+    { name: '整理脈絡', value: 'a' },
+    { name: '批判', value: 'b' },
+    { name: '綜合', value: 'c' },
+  ];
 
   askQuestion(): void {
     this.isLoaded = true;
+    const {question,precondition}=this.askForm.value;
     this.askService
-      .askGemini(this.currentQuestion)
+      .askGemini2((question as string), (precondition as string))
       .pipe(finalize(() => (this.isLoaded = false)))
       .subscribe((response) => {
         console.log(response);
--- src/app/service/ask.service.ts
+++ src/app/service/ask.service.ts
@@ -7,11 +7,20 @@ import { Observable } from 'rxjs';
 })
 export class AskService {
   private http=inject(HttpClient);
+  private url='https://think-therefore-be-api.azurewebsites.net/gemini';
   constructor() { }
 
   askGemini(prompt:string):Observable<string>{
     const url=`https://think-therefore-be-api.azurewebsites.net/gemini?prompt=${prompt}`;
     // return this.http.get<string>(url, { responseType: 'text' });
     return this.http.get(url, { responseType: 'text' });
+  }
+
+  askGemini2(question:string,precondition:string):Observable<string>{
+    const body={
+      prompt:question,
+      precondition
+    }
+    return this.http.post<string>(this.url,body, { responseType: 'text' as any });
   }
 }
