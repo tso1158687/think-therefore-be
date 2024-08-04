57c8d3a8c6329e393840daf4da22bd19c8571654
jason zheng <tso1158687@gmail.com>
2024-08-04 12:54:37 +0800

    add ngx-translate for 18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,21 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './component/header/header.component';
 import { MarkdownModule } from 'ngx-markdown';
 import { SidebarModule } from 'primeng/sidebar';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MarkdownModule, SidebarModule],
+  imports: [RouterOutlet, HeaderComponent, MarkdownModule, SidebarModule, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private translateService: TranslateService) {
+    this.translateService.setDefaultLang('en');
+    this.translateService.addLangs(['en', 'zh-tw']);
+    this.translateService.setDefaultLang('zh-tw');
+
+    const browserLang = this.translateService.getBrowserLang();
+    this.translateService.use(browserLang?.match(/en|zh-tw/) ? browserLang : 'zh-tw');
+  }
+}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,29 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 import { provideMarkdown } from 'ngx-markdown';
 import { provideLottieOptions } from 'ngx-lottie';
-
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './i18n/', '.json');
+}
+export const provideTranslation = () => ({
+  loader: {
+    provide: TranslateLoader,
+    useFactory: HttpLoaderFactory,
+    deps: [HttpClient],
+  },
+});
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
+    importProvidersFrom([TranslateModule.forRoot(provideTranslation())]),
     provideHttpClient(withFetch()),
     provideMarkdown(),
     provideLottieOptions({
