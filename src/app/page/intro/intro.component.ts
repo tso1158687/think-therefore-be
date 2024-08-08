4acf80eb79f1bffecf40cc5b7414ea8ac1811001
jason zheng <tso1158687@gmail.com>
2024-08-09 00:29:50 +0800

    adjust UI style

--- src/app/page/intro/intro.component.ts
+++ src/app/page/intro/intro.component.ts
@@ -2,11 +2,18 @@ import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { AnimationOptions, LottieComponent } from 'ngx-lottie';
 import { CardModule } from 'primeng/card';
-
+import { ButtonModule } from 'primeng/button';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-intro',
   standalone: true,
-  imports: [LottieComponent, CardModule,TranslateModule],
+  imports: [
+    LottieComponent,
+    CardModule,
+    TranslateModule,
+    ButtonModule,
+    RouterModule,
+  ],
   templateUrl: './intro.component.html',
   styleUrl: './intro.component.scss',
 })
